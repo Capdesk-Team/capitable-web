bcc50f6ddad92bedf5d5887f825534477a6853ba
Takato Noguchi <takato.noguchi23@gmail.com>
2023-12-11 21:16:50 +0900

    dashboards ui fix

--- frontend/src/components/pages/Dashboards.tsx
+++ frontend/src/components/pages/Dashboards.tsx
@@ -1,132 +1,155 @@
-import * as React from 'react';
+// React
+import React, { useState, useContext, useEffect } from "react"
+import { Link } from 'react-router-dom';
+import { AuthContext } from "App"
+// API
+import { getProject } from 'api/project'
+// Interfaces
+import { getProjectList } from 'interfaces/project'
+// Material UI
 import AppBar from '@material-ui/core/AppBar';
 import Box from '@material-ui/core/Box';
 import CssBaseline from '@material-ui/core/CssBaseline';
-import Divider from '@material-ui/core/Divider';
 import Drawer from '@material-ui/core/Drawer';
-import IconButton from '@material-ui/core/IconButton';
 import List from '@material-ui/core/List';
-import ListItem from '@material-ui/core/ListItem';
-import ListItemButton from '@material-ui/core/ListItem';
-import ListItemIcon from '@material-ui/core/ListItemIcon';
-import ListItemText from '@material-ui/core/ListItemText';
-import MailIcon from '@material-ui/icons/Mail';
-import MenuIcon from '@material-ui/icons/Menu';
 import Toolbar from '@material-ui/core/Toolbar';
-import InboxIcon from '@material-ui/icons/MoveToInbox';
 import Card from "@material-ui/core/Card"
 import Typography from "@material-ui/core/Typography"
 import CardContent from "@material-ui/core/CardContent"
-import { Link } from "react-router-dom";
 import Button from "@material-ui/core/Button"
+import {Grid}  from "@material-ui/core"
+import Divider from '@material-ui/core/Divider'
+// Styles
+import { makeStyles, Theme } from "@material-ui/core/styles"
+// Components
+import { dashboardListItems } from './DashboardListItems';
 
 const drawerWidth = 240;
 
-interface Props {
-  /**
-   * Injected by the documentation to work in an iframe.
-   * Remove this when copying and pasting into your project.
-   */
-  window?: () => Window;
-}
+const useStyles = makeStyles((theme: Theme) => ({
+  link: {
+    textDecoration: "none",
+    color: "blue",
+    "&:hover, &:active, &:focus": {
+      color: "blue",
+      textDecoration: "none", 
+    }
+  },
+}))
 
-const Dashboard: React.FC = (props: Props) => {
-  const { window } = props;
-  const [mobileOpen, setMobileOpen] = React.useState(false);
+const Dashboard: React.FC = () => {
 
-  const handleDrawerToggle = () => {
-    setMobileOpen(!mobileOpen);
-  };
+  const { isSignedIn, currentUser } = useContext(AuthContext)
+  const [projectList, setProjectList ] = useState<getProjectList[]>([]);
 
-  const drawer = (
-    <div>
-      <Toolbar />
-      <Divider />
-      <List>
-        {['ホーム', 'スカウト', '応募者確認'].map((text, index) => (
-          <ListItem key={text} >
-            <ListItemButton>
-              <ListItemIcon>
-                {index % 2 === 0 ? <InboxIcon /> : <MailIcon />}
-              </ListItemIcon>
-              <ListItemText primary={text} />
-            </ListItemButton>
-          </ListItem>
-        ))}
-      </List>
-    </div>
-  );
+  const classes = useStyles()
 
-  // Remove this const when copying and pasting into your project.
-  const container = window !== undefined ? () => window().document.body : undefined;
+  useEffect(() => {
+    handleGetProject();
+  }, []); 
 
-  return (
-    <Box sx={{ display: 'flex' }}>
-      <CssBaseline />
-      <AppBar
-        position="fixed"
-        color="default"
-      >
-        <Toolbar>
-          <IconButton
-            color="inherit"
-            aria-label="open drawer"
-            edge="start"
-            onClick={handleDrawerToggle}
-          >
-            <MenuIcon />
-          </IconButton>
-         
-        </Toolbar>
-      </AppBar>
-      <Box
-        component="nav"
-        sx={{ width: { sm: drawerWidth }, flexShrink: { sm: 0 } }}
-        aria-label="mailbox folders"
-      >
-        {/* The implementation can be swapped with js to avoid SEO duplication of links. */}
-        <Drawer
-          container={container}
-          variant="temporary"
-          open={mobileOpen}
-          onClose={handleDrawerToggle}
-          ModalProps={{
-            keepMounted: true, // Better open performance on mobile.
-          }}
+  const handleGetProject = async () => {
+    try {
+      const res = await getProject()
+      console.log(res.data)
+      setProjectList(res.data)
+    } catch(e) {
+      console.log(e);
+    }
+  }
+
   
-        >
-          {drawer}
-        </Drawer>
-        <Drawer
-          variant="permanent"
-          open
-        >
-          {drawer}
-        </Drawer>
-      </Box>
-      <Box
-        component="main"
-        sx={{ flexGrow: 1, p: 3, width: { sm: `calc(100% - ${drawerWidth}px)` } }}
-      >
-        <h3>プロジェクト一覧で自分が作成したプロジェクトを表示</h3>
-        <Card>
-        <CardContent>
-          <Typography variant="h5" component="div">
-            プロジェクトタイトル
-          </Typography>
-          <Button
-              component={Link}
-              to={'/'}
-              color="primary"
-              variant="outlined"
-            >
-              応募者を確認する
-            </Button>
-        </CardContent>
-        </Card>
+  return (
+    <>
+      <Box sx={{ display: 'flex' }}>
+        <CssBaseline />
 
+        {/* ナビゲーションバー */}
+        <AppBar
+          position="fixed"
+          color="default"
+        >
+          <Toolbar />
+        </AppBar>
+
+        {/* サイドメニューバー */}
+        <Box
+          component="nav"
+          sx={{ width: { sm: drawerWidth }, flexShrink: { sm: 0 } }}
+          aria-label="mailbox folders"
+        >
+          <Drawer
+            variant="permanent"
+            open
+          >
+            <Toolbar />
+            <Divider />
+            <List>
+              {dashboardListItems}
+            </List>
+          </Drawer>
+        </Box>
+              
+              
+        {/* 自分が作成したプロジェクト */}
+        <Box
+          component="main"
+          sx={{ flexGrow: 1, p: 3, width: { sm: `calc(100% - ${drawerWidth}px)` } }}
+        >
+          {
+            isSignedIn && currentUser ? (
+            <>
+              <Typography
+                variant="h6"
+              >
+                作成した募集一覧
+              </Typography>
+
+              <Grid container spacing={4} >
+                {projectList.map((project: getProjectList, index) => (
+                  <React.Fragment key={index}>
+                      {currentUser.id === project.userId ? (
+                      <Grid key={index} item xs={12} md={4}>
+                        <Card>
+                          <CardContent>
+                            <Typography
+                              variant="h6"
+                            >
+                              募集内容
+                            </Typography>
+                            <Typography>
+                              {project.title}
+                            </Typography>
+                            <Button
+                              component={Link}
+                              to={`/project/${project.id}/apply`}
+                              color="primary"
+                              variant="outlined"
+                            >
+                              応募者を確認する
+                            </Button>
+                          </CardContent>
+                        </Card>
+                      </Grid>
+                      ) : null}
+                  </React.Fragment>
+                ))}
+              </Grid>
+            </>
+            ) : (
+              <>
+                <h2>ログインが必要です</h2>
+                <Button>
+                  <Link to="/signin" className={classes.link}>
+                    ログインはこちらから
+                  </Link>
+                </Button>
+              </>
+            )
+          }
+        </Box>
       </Box>
-    </Box>
+    </>
   );
 }
 

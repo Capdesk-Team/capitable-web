7adc00c2f8aff7640262d80e196a12b3ff56aa70
Takato Noguchi <takato.noguchi23@gmail.com>
2023-12-11 20:29:59 +0900

    terms & privacy page add

---
+++ frontend/src/components/pages/Terms.tsx
@@ -0,0 +1,22 @@
+import React from 'react'
+
+const Terms = () => {
+  return (
+      <>
+        <div>
+          <h2>利用規約</h2>
+          <div>
+            <span>第1条(適用)</span>
+            <ul>
+              <li>
+                本規約は、本サービスの提供条件及び本サービスの利用に関する当社と登録ユーザーとの間の権利義務関係を定めることを目的とし、
+                登録ユーザーと当社との間の本サービスの利用に関わる一切の関係に適用されます。
+              </li>
+            </ul>
+          </div>
+        </div>
+      </>
+  )
+}
+
+export default Terms

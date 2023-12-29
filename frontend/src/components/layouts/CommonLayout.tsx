cea60348ecf926198f62fbabd5b76445bacab588
Takato Noguchi <takato.noguchi23@gmail.com>
2023-12-29 16:19:59 +0900

    fix footer

--- frontend/src/components/layouts/CommonLayout.tsx
+++ frontend/src/components/layouts/CommonLayout.tsx
@@ -4,8 +4,16 @@ import { Container, Grid } from "@material-ui/core"
 import { makeStyles } from "@material-ui/core/styles"
 
 import Header from "components/layouts/Header"
+import Footer from "components/layouts/Footer"
 
 const useStyles = makeStyles(() => ({
+  wrapper: {
+    height: '100%',
+    minHeight: '100vh',
+    position: 'relative',
+    paddingBottom: 120,
+    boxSizing: "border-box",
+  },
   container: {
     marginTop: "3rem"
   },
@@ -21,18 +29,19 @@ const CommonLayout = ({ children }: CommonLayoutProps) => {
 
   return (
     <>
-      <header>
-        <Header/>
-      </header>
-      <main>
-        <Container maxWidth="lg" className={classes.container}>
-          <Grid container justify="center">
-            <Grid item>
-              {children}
-            </Grid>   
-          </Grid>
-        </Container>
-      </main>
+      <div className={classes.wrapper}>
+        <header>
+          <Header/>
+        </header>
+        <main>
+          <Container maxWidth="lg" className={classes.container}>
+            {children}
+          </Container>
+        </main>
+        <footer>
+          <Footer/>
+        </footer>
+      </div>
     </>
   )
 }

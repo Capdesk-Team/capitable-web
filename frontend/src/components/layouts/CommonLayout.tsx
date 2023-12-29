eafe915520c178556e71b48f3699a4d2c754e4e4
Takato Noguchi <takato.noguchi23@gmail.com>
2023-12-29 16:20:36 +0900

    fix footer

--- frontend/src/components/layouts/CommonLayout.tsx
+++ frontend/src/components/layouts/CommonLayout.tsx
@@ -1,6 +1,6 @@
 import React from "react"
 
-import { Container, Grid } from "@material-ui/core"
+import { Container } from "@material-ui/core"
 import { makeStyles } from "@material-ui/core/styles"
 
 import Header from "components/layouts/Header"

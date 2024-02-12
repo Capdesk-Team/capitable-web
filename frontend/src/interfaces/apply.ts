9874a2241edf5d3af747dadc284e957178db7973
Takato Noguchi <takato.noguchi23@gmail.com>
2024-02-12 14:42:04 +0900

    add and update

--- frontend/src/interfaces/apply.ts
+++ frontend/src/interfaces/apply.ts
@@ -1,10 +1,10 @@
 // 応募してきたユーザー一覧
 export interface ApplyUsers {
-  id: string
-  userId: string
-  projectId: string
+  id: number
+  userId: number
+  jobId: number
   user: {
-    id: string
+    id: number
     name: string
     techSkill: string
     portfolioUrl: string
@@ -16,14 +16,14 @@ export interface ApplyUsers {
 
 //応募してきたユーザーを個別で表示
 export interface ShowApplyUser {
-  id: string 
-  userId: string
-  projectId: string
+  id: number 
+  userId: number
+  jobId: number
 }
 
 // プロジェクトへの応募
 export interface Apply {
-  id?: string
-  userId: string | undefined
-  projectId: string
+  id?: number
+  userId: number | undefined
+  jobId: number
 }

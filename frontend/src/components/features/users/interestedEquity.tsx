4dc14dd2ba0e22af893f538fd3f95d26988a91e5
Takato Noguchi <takato.noguchi23@gmail.com>
2024-02-12 15:24:38 +0900

    delete user profile edit

--- frontend/src/components/features/users/interestedEquity.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const interestedEquity = () => {
-  return (
-    <div>interestedEquity</div>
-  )
-}
-
-export default interestedEquity
--- frontend/src/components/features/users/receiveScout.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const recieveScout = () => {
-  return (
-    <div>recieveScout</div>
-  )
-}
-
-export default recieveScout
